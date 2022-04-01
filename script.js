86ab6b181535b1662ca16e9d47f62d2db93c2008
rrrussrrr <79482980+rrrussrrr@users.noreply.github.com>
2022-04-01 11:25:58 -0400

    Added playRound() function that returns result

--- script.js
+++ script.js
@@ -15,5 +15,31 @@ function computerPlay() {
     }   
 }
 
-let computer = computerPlay();
-console.log(computer);
+function playRound(playerSelection, computerSelection) {
+
+    let pl
+    let winBool = false;
+    if (playerSelection === computerSelection) {
+        return `TIE!  You both picked ${playerSelection}!`;
+    } else if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return `LOSE!  ${playerSelection} loses to ${computerSelection}!`;
+        } else return `WIN!  ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Scissors') {
+            return `LOSE!  ${playerSelection} loses to ${computerSelection}!`;
+        } else return `WIN!  ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            return `LOSE!  ${playerSelection} loses to ${computerSelection}!`;
+        } else return `WIN!  ${playerSelection} beats ${computerSelection}!`;
+    }
+}
+
+let playerChoice = 'Rock';
+let computerChoice = computerPlay();
+let message;
+console.log(playerChoice);
+console.log(computerChoice);
+message = playRound(playerChoice, computerChoice);
+console.log(message);
