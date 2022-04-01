2a7eeb7713e5d47751f6d68350565c5a0e7367c4
rrrussrrr <79482980+rrrussrrr@users.noreply.github.com>
2022-04-01 10:52:54 -0400

    added randomInt and computerPlay functions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function randomInt(min,max) {
+    return randomNumber = Math.floor(Math.random() * (max - min +1) + min);
+    
+}
+
 function computerPlay() {
 
+    let computerChoice = randomInt(1,3);
+    return computerChoice;
+    
 }
+
+let computer = computerPlay();
+console.log(computer);
