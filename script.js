a320d3f76841c7bd25c81803dcf797762e3b63a7
rruebner <rruebner@gmail.com>
2022-05-03 03:06:36 -0400

    rewrote code to play game on button click

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function randomInt(min,max) {
     return randomNumber = Math.floor(Math.random() * (max - min +1) + min);
-    
 }
 
 function computerPlay() {
-
     let compChoose = randomInt(1,3);
     if (compChoose === 1) {
         return 'Rock';
@@ -15,78 +13,54 @@ function computerPlay() {
     }   
 }
 
-function playRound(playerSelection, computerSelection) {
+var playerWinCount = 0;
+var computerWinCount = 0;
+var gameOver = false;
 
+function playRound(e) {
+
+    if (gameOver === true){
+        document.getElementById('game-over').textContent = ``;
+        gameOver = false;
+        playerWinCount = 0;
+        computerWinCount = 0;
+        document.getElementById('compwins').textContent = computerWinCount;
+        document.getElementById('playerwins').textContent = playerWinCount;
+    }
+
+    const computerSelection = computerPlay();
+    const playerSelection = e.currentTarget.id;
     if ((playerSelection === 'Rock' && computerSelection == 'Scissors') ||
         (playerSelection === 'Scissors' && computerSelection == 'Paper') ||
         (playerSelection === 'Paper' && computerSelection == 'Rock')) {
             playerWinCount++;
-            return `WIN!  ${playerSelection} beats ${computerSelection}!`;
+            console.log(playerWinCount);
+            document.getElementById('playerwins').textContent = playerWinCount;
+            document.getElementById('result').textContent = `WIN!  ${playerSelection} beats ${computerSelection}!`;
         }
         else if (playerSelection === computerSelection) {
-            return `TIE!  You both picked ${playerSelection}!`;
+            document.getElementById('result').textContent =`TIE!  You both picked ${playerSelection}!`;
         }
          else {
              computerWinCount++;
-            return `LOSE!  ${playerSelection} loses to ${computerSelection}!`;
+             document.getElementById('compwins').textContent = computerWinCount;
+             document.getElementById('result').textContent = `LOSE! ${playerSelection} loses to ${computerSelection}!`;
         }
 
+        
+        if (playerWinCount >= 5) {
+            gameOver = true;
+            document.getElementById('game-over').textContent = `GAME OVER!  Player wins!`;
+        }
+
+        if (computerWinCount >= 5) {
+            gameOver = true;
+            document.getElementById('game-over').textContent = `GAME OVER!  Computer wins!`;
+        }
+
+
 }
 
-
-
-function capitalizeFirst(string){
-    string = string.toLowerCase();
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-// playerInputCheck
-// Checks if input is rock, paper, or scissors.  Defaults to Rock in a conflict
-function playerInputCheck(input) {
-    if (input === 'Rock' || input === 'Paper' || input === 'Scissors' ) {
-        return input;
-    } else {
-        alert("Invalid choice! Defaulting to Rock");
-        return 'Rock';
-    }
-}
-
-let playerWinCount;
-let computerWinCount;
-let computerChoice;
-let playerChoice;
-
-function game() {
-
-    playerWinCount = 0;
-    computerWinCount = 0;
-    computerChoice = 0;
-    playerChoice = 0;
-
-    for (i = 0; i < 5; i++) {
-        // resetting values
-        // Making choices
-        computerChoice = computerPlay(); // computer pick
-        console.log(computerChoice);
-        playerChoice = window.prompt("Rock, Paper, or Scissors?"); // user pick
-        console.log(playerChoice);
-        playerChoice = capitalizeFirst(playerChoice); // formatting input
-        console.log(playerChoice);
-        playerChoice = playerInputCheck(playerChoice); // checking valid input
-        console.log(playerChoice);
-
-        console.log(playRound(playerChoice, computerChoice)); // play!
-        console.log(computerWinCount);
-        console.log(playerWinCount);
-
-    }
-
-    if (playerWinCount > computerWinCount) {
-        console.log('You win!');
-    } else if (playerWinCount < computerWinCount){
-        console.log('You lose!')
-    } else console.log(`It's a draw!`);
-
-    
-}
+const choices = document.querySelectorAll('.choice');
+choices.forEach(choice => {choice.addEventListener('click', playRound)});
 
